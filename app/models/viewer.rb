4ac0f7812cfd9af0624642e2a9639c75912c169e
github username <github email address>
2019-04-19 10:58:02 -0500

    done

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -5,11 +5,30 @@ class Viewer
 
   def initialize(username)
     @username = username
-    self.class.all << self
+    @@all << self
   end
 
   def self.all
     @@all
   end
-  
+
+  def queue_movies
+    queue_movies.all.select do |movie|
+      movie.viewer == self
+      end
+    end
+
+    def add_movie_to_queue
+
+
+
 end
+
+# + `Viewer#queue_items`
+#   + this method should return an array of `QueueItem` instances associated with this instance of `Viewer`.
+# + `Viewer#queue_movies`
+#   + this method should return an array of `Movie` instances in the `Viewer`'s queue.
+# + `Viewer#add_movie_to_queue(movie)`
+#   + this method should receive a `Movie` instance as its only argument and add it to the `Viewer`'s queue
+# + `Viewer#rate_movie(movie, rating)`
+#   + given a movie and a rating (a number between 1 and 5), this method should assign the rating to the viewer's `QueueItem` for that movie. If the movie is not already in the viewer's queue, this method should add a new `QueueItem` with the viewer, movie, and rating. If the movie is already in the queue, this method should not create a new `QueueItem`.
